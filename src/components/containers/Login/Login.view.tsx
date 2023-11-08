ba79934999c59644593bb09d915208d51578eafe
Amir Ben Shimol <amir.benshi@gmail.com>
2023-11-08 16:14:04 +0100

    add usage of env exmaple

--- src/components/containers/Login/Login.view.tsx
+++ src/components/containers/Login/Login.view.tsx
@@ -5,10 +5,11 @@ import classes from './Login.module.scss';
 type TProps = object;
 
 const LoginView = () => {
+	console.log('backend url', import.meta.env.VITE_BACKEND_URL);
+
 	return (
 		<section className={classes['container']}>
 			<div>login page</div>
-			<button type="button" />
 		</section>
 	);
 };
