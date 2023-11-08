0ffebaf364e2ce183b63ad05128bb48c175eaf16
Amir Ben Shimol <amir.benshi@gmail.com>
2023-11-08 11:18:46 +0100

    configure environment

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import path from "node:path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import sassDts from "vite-plugin-sass-dts";
+
+export default defineConfig(({ mode }) => {
+	const appMode = process.env.AUTOMATION === "true" ? "test" : mode;
+
+	return {
+		server: {
+			port: 8080,
+		},
+		base: "/",
+		build: { outDir: "./dist" },
+		plugins: [react(), tsconfigPaths(), sassDts()],
+		resolve: { alias: { "@/styles": path.join(__dirname, "src", "styles") } },
+
+		preview: {
+			port: 8080,
+		},
+		mode: appMode,
+	};
+});
